c40da3e166691da2cbc3476abf29bd247d4a6f8c
jobitoalv <74395645+jobitoalv@users.noreply.github.com>
2021-05-19 11:00:09 +1000

    added add on services

---
+++ Classes/add_on.rb
@@ -0,0 +1,32 @@
+require_relative 'service'
+
+class Add_on
+    attr_reader :name, :price
+
+    def initialize(name, details, price)
+        @name = name
+        @details = details
+        @price = '%.2f' % price 
+    end
+
+    def display_add_on
+        puts "Add On service: #{@name}"
+        puts "Price: $#{@price}"
+        puts 
+        puts "Details: #{@details}"
+    end
+end 
+
+class Spa < Add_on
+    def initialize
+         super("Dog Facial", "Cleanse, Nourishes and hydrates your dog`s skin using aromacare dog facial cleanser ", 30)
+    end
+end
+
+class Ear < Add_on
+    def initialize
+         super("Deep Ear Clean", "Ear drops are massaged into dog`s ear canals to help loosen uo any gunk and provide a thorough cleanse.", 19)
+    end
+end
+
+class 
